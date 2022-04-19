2b2175b5b89554a6f4b6e4a234f17f61df038d40
souayrioss <souayri.oussama@gmail.com>
2022-04-19 04:10:40 +0000

    edit and deletge contact function

--- class/contact.php
+++ class/contact.php
@@ -19,16 +19,6 @@
             $this->address = $address ;
             $this->id_user = $id_user;
         }
-        public function getInfoContacts( $firstName, $lastName,$phone ,$email, $address,$id_user)
-        {
-            return $this->firstName = $firstName ;
-            return $this->lastName = $lastName ;
-            return $this->phone = $phone ;
-            return $this->email = $email ;
-            return $this->address = $address ;
-            return $this->id_user = $id_user;
-        }
-
         public function addContact(){
             $conn = $this->connect();
             $sql ="INSERT INTO contacts(firstName, lastName, phone, email,address,id_user ) VALUES (?,?,?,?,?,? )";
@@ -41,7 +31,8 @@
                 $this->address,
                 $this->id_user 
             ]);
-            header('location :contact.php');
+            echo '<script>window.location="contact.php"</script>';
+
         }
         public function getConatctsByIdUser($id_user){
             $conn = $this->connect();
@@ -56,13 +47,35 @@
                             <td class="px-6 py-4 border" ><?= $contact['email'] ?></td>
                             <td class="px-6 py-4 border" ><?= $contact['phone'] ?></td>
                             <td class="px-6 py-4 border" ><?= $contact['address'] ?></td>
-                            <td class="px-6 py-4 border " ><button class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="">Edit</button></td>
-                            <td class="px-6 py-4 border" ><a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="delete.php?id=<?= $contact['id']?>">Delete</a></td>
+                            <td class="px-6 py-4 border " ><form method="POST" action="editContact.php"><input type="hidden" name="id" value="<?= $contact['id']?>"><input class="font-medium text-blue-600 dark:text-blue-500 hover:underline cursor-pointer" type="submit"value="Edit"></form></td>
+                            <td class="px-6 py-4 border" ><a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="deleteContact.php?id=<?= $contact['id']?>">Delete</a></td>
                         </tr>
                 <?php
             }
         }
-        public function deleteContactById(){
+        public function getConatctById($id){
+            $conn = $this->connect();
+            $stm = $conn->prepare("SELECT * FROM contacts WHERE id = :id");
+            $stm->bindParam(":id", $id, PDO::PARAM_STR);
+            $stm->execute(); 
+            $contacts = $stm->fetch(PDO::FETCH_ASSOC);
+            if($contacts){
+                return $contacts;
+            }
+        }
+        public function editContact($id,$firstName,$lastName,$phone,$email,$address){
+            $conn = $this->connect();
+            $stm = $conn->prepare("UPDATE contacts SET firstName = :firstName ,lastName = :lastName,phone = :phone ,email= :email ,address = :address WHERE id = :id");
+            $stm->execute([
+                ':firstName'=> $firstName,
+                ':lastName'=> $lastName,
+                ':phone'=> $phone,
+                ':email'=> $email,
+                ':address'=> $address,
+                ':id'=> $id
+            ]); 
+        }
+        public function deleteContactById($id){
             $conn = $this->connect();
             $stm = $conn->prepare("DELETE  FROM contacts WHERE id = :id");
             $stm->bindParam(":id", $id, PDO::PARAM_STR);
